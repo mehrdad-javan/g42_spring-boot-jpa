b90fc2fb2b2921528bb2dacc262d583a1c475b56
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-06-08 10:37:05 +0200

    create Student Class define Student class as an Entity or Table rename the model package to entity

---
+++ src/main/java/se/lexicon/demo/entity/Student.java
@@ -0,0 +1,134 @@
+package se.lexicon.demo.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+//@Table(name = "TBL_STUDENT")
+public class Student { // TBL_STUDENT
+
+    @Id // primary key for id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // means auto_increment
+    private int id;
+
+    @Column(nullable = false, length = 100) // not null varchar(100)
+    private String firstName;
+
+    @Column(nullable = false, length = 100)
+    private String lastName;
+
+    @Column(nullable = false, length = 200, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    private boolean status;
+
+    private LocalDateTime registerDate;
+
+    public Student() {
+        this.registerDate = LocalDateTime.now();
+    }
+
+    public Student(String firstName, String lastName, String email, LocalDate birthDate) {
+        this();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public Student(int id, String firstName, String lastName, String email, LocalDate birthDate) {
+        this();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(LocalDateTime registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && status == student.status && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(email, student.email) && Objects.equals(birthDate, student.birthDate) && Objects.equals(registerDate, student.registerDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, birthDate, status, registerDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", status=" + status +
+                ", registerDate=" + registerDate +
+                '}';
+    }
+}
